b331417613990c18385aad46c62c7831ec2e21aa
Clarke Lamothe <clarkelamothe@yahoo.fr>
2021-02-25 01:28:12 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Dados';
+  rightDice = './assets/img/dice1.svg';
+  leftDice = './assets/img/dice5.svg';
+
+  number1: number = 1;
+  number2: number = 2;
+
+  rollDices(): void {
+    this.number1 = Math.round(Math.random() * 5) + 1;
+    this.number2 = Math.round(Math.random() * 5) + 1;
+    this.rightDice = './assets/img/dice' + this.number1 + '.svg';
+    this.leftDice = './assets/img/dice' + this.number2 + '.svg';
+  }
 }
